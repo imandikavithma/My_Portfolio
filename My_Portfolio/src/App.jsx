cd7599d22225055dd9c58952bddc95d628e3aa3c
Imandi <imandikavithma2714@gmail.com>
2025-05-16 22:55:26 +0530

    Added Hero section

--- My_Portfolio/src/App.jsx
+++ My_Portfolio/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Navbar from './components/navbar'
+import Home from './components/home'
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <Navbar/>
+      <Home/>
     </div>
   )
 }
