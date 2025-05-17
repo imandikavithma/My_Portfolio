517557bfc94d45124eeb67aea3ae1dac2cb8f801
Imandi <imandikavithma2714@gmail.com>
2025-05-17 10:57:28 +0530

    Added about section

--- My_Portfolio/src/App.jsx
+++ My_Portfolio/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Navbar from './components/navbar'
 import Home from './components/home'
+import About from './components/about'
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Home/>
+      <About/>
     </div>
   )
 }
